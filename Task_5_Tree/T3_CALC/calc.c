a7adea333a1ae634e190ebf32fd4508458e9a952
bearbattle <bearbattle@users.noreply.github.com>
2019-05-07 00:40:50 +0800

    完成了第3题

--- Task_5_Tree/T3_CALC/calc.c
+++ Task_5_Tree/T3_CALC/calc.c
@@ -1,43 +1,240 @@
+/*************************
+*   Title:  calc - Calculator
+*   Author: BearHuchao
+*   Data:   2019.4.26-5.7
+*   Note:   Achieved by Tree
+*************************/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXN 1024
+
 typedef struct Tree
 {
+    int data;
+    int type; //0 for number, 1 for operator
     struct Tree *left, *right;
-    int data, type; //type:0 for num; 1 for operator
 } tree;
 
-tree *stack[1024];
+tree *root;
+tree *stack[MAXN];
+int top = -1;
 
-void Build();
+tree *Pop();
+
+void Push(tree *src);
+
+void TreeSet(char *s);
+
+void Transform(char *des, const char *src);
+
+int Calc(tree *des);
+
+void Print();
 
 int main()
 {
-    Build();
+    char src[1024], post[1024];
+    memset(src, 0, sizeof(src));
+    gets(src);
+    Transform(post, src);
+    TreeSet(post);
+    Print();
+    printf("%d", Calc(root));
+    return 0;
 }
 
-void Build()
+void TreeSet(char *s)
 {
-    char Buf[1024];
-    char *s;
-    int num;
-    fgets(Buf, 1000, stdin);
-    for (s = Buf;; s++)
+    int num = 0;
+    tree *p, *q;
+    tree *new;
+
+    while (*s)
     {
-        if (isdigit(*s))
+        switch (*s)
         {
-            num = num * 10 + *s - '0';
-            continue;
-        }
-        if (num != 0)
-        {
-            NUMStack[++NUM] = num;
-        }
-        num = 0;
-        Check(*s);
-        if (*s == '\0')
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            new = (tree *)malloc(sizeof(tree));
+            p = Pop();
+            q = Pop();
+            new->type = 1;
+            new->data = *s;
+            new->left = q;
+            new->right = p;
+            Push(new);
             break;
+
+        default:
+            while (isdigit(*s))
+            {
+                num = num * 10 + *s - '0';
+                s++;
+            }
+            if (num != 0)
+            {
+                new = (tree *)malloc(sizeof(tree));
+                new->type = 0;
+                new->data = num;
+                new->left = NULL;
+                new->right = NULL;
+                Push(new);
+                num = 0;
+                s--;
+            }
+        }
+        s++;
+    }
+    root = stack[0];
+};
+
+tree *Pop()
+{
+    if (top != -1)
+        return stack[top--];
+    return NULL;
+}
+
+void Push(tree *src)
+{
+    stack[++top] = src;
+}
+
+void Transform(char *des, const char *src)
+{
+    char OPStack[MAXN];
+    int num = 0, OP = -1;
+    memset(des, 0, MAXN);
+    memset(OPStack, 0, sizeof(OPStack));
+    while (*src)
+    {
+        /********************************
+        符号    优先级，大的先算，小的先进栈
+        (       0
+        + -     1
+        * / %   2
+        )       3
+        ********************************/
+        switch (*src)
+        {
+        case ' ':
+            break;
+
+        case '+':
+        case '-':
+            while ((OPStack[OP] == '+' || OPStack[OP] == '-' || OPStack[OP] == '*' || OPStack[OP] == '/') &&
+                   OP != -1)
+            {
+                sprintf(des + strlen(des), "%c", OPStack[OP--]);
+            }
+            OPStack[++OP] = *src;
+            break;
+
+        case '*':
+        case '/':
+            while ((OPStack[OP] == '*' || OPStack[OP] == '/') && OP != -1)
+            {
+                sprintf(des + strlen(des), "%c", OPStack[OP--]);
+            }
+            OPStack[++OP] = *src;
+            break;
+
+        case '(':
+            OPStack[++OP] = *src;
+            break;
+
+        case ')':
+            while (OPStack[OP] != '(')
+            {
+                sprintf(des + strlen(des), "%c", OPStack[OP--]);
+            }
+            OP--;
+            break;
+
+        default:
+            while (isdigit(*src))
+            {
+                num = num * 10 + *src - '0';
+                src++;
+            }
+            if (num != 0)
+            {
+                sprintf(des + strlen(des), "%d ", num);
+                num = 0;
+                src--;
+            }
+        }
+        src++;
+    }
+    while (OP != -1)
+    {
+        sprintf(des + strlen(des), "%c", OPStack[OP--]);
+    }
+}
+
+int Calc(tree *des)
+{
+    if (des->type == 0)
+        return des->data;
+    if (des->type == 1)
+    {
+        switch (des->data)
+        {
+        case '+':
+            return Calc(des->left) + Calc(des->right);
+
+        case '-':
+            return Calc(des->left) - Calc(des->right);
+
+        case '*':
+            return Calc(des->left) * Calc(des->right);
+
+        case '/':
+            return Calc(des->left) / Calc(des->right);
+
+        default:
+            return 0;
+        }
+    }
+    return 0;
+}
+
+void Print()
+{
+    tree *tmp;
+    tmp = root;
+    switch (tmp->type)
+    {
+    case 0:
+        printf("%d ", tmp->data);
+        break;
+    case 1:
+        printf("%c ", tmp->data);
+        break;
+    }
+    tmp = root->left;
+    switch (tmp->type)
+    {
+    case 0:
+        printf("%d ", tmp->data);
+        break;
+    case 1:
+        printf("%c ", tmp->data);
+        break;
+    }
+    tmp = root->right;
+    switch (tmp->type)
+    {
+    case 0:
+        printf("%d\n", tmp->data);
+        break;
+    case 1:
+        printf("%c\n", tmp->data);
+        break;
     }
 }
