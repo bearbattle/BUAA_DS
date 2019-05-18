927cdf64eb6dc4c709bace30520cb6c4322fd068
bearbattle <bearbattle@users.noreply.github.com>
2019-05-18 09:46:29 +0800

    basis

---
+++ Task_5_Tree/T5_HUFFMAN/huffman.c
@@ -0,0 +1,10 @@
+/*************************
+*   Title:  Huffman
+*   Author: BearHuchao
+*   Data:   2019.5.18
+*   Note:   Compress File
+*************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
