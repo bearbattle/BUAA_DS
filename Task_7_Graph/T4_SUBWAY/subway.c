deedc486f0bc3dd2a15efc1525ec4cde35ddde2b
bearbattle <chao4150443@gmail.com>
2019-06-30 21:22:30 +0800

    地铁写完啦！！！！

--- Task_7_Graph/T4_SUBWAY/subway.c
+++ Task_7_Graph/T4_SUBWAY/subway.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INF 0x7FFFFFFF
+#define INF 0x2FFFFFFF
 
 typedef struct Edge
 {
@@ -22,25 +22,49 @@ typedef struct Node
 
 node map[501];
 int path[501][501];
-int destination;
+int startpoint, destination;
 int CountOfStation = 0;
 
 void Init();
 
-void Dijkstra();
+void Dijkstra(char *Start, char *Des);
 
 int main()
 {
     int i;
+    int step = 0;
     char beg[50], end[50];
+    int curLine, curCount;
+    char routine[501];
+    char station[50];
     Init();
+    //Out();
     scanf("%s", beg);
     scanf("%s", end);
-    Dijkstra(beg, end);
+    Dijkstra(end, beg);
     for (i = 0; path[destination][i] != -1; i++)
     {
-        printf("%s ", map[path[destination][i]].Name);
+        step++;
     }
+    curLine = map[path[destination][step - 1]].from->line;
+    curCount = 1;
+    printf("%s", map[path[destination][step - 1]].Name);
+    for (i = step - 2; i >= 1; i--)
+    {
+        //printf("%s(%d) ", map[path[destination][i]].Name, (map[path[destination][i]].from)->line);
+        if (map[path[destination][i]].from->line == curLine)
+            curCount++;
+        else
+        {
+            printf("-%d(%d)-", curLine, curCount);
+            printf("%s", map[path[destination][i]].Name);
+            curLine = map[path[destination][i]].from->line;
+            curCount = 0;
+            curCount++;
+        }
+    }
+    printf("-%d(%d)-", curLine, curCount);
+    printf("%s", map[path[destination][0]].Name);
     return 0;
 }
 
@@ -51,11 +75,12 @@ void Init()
     int SumOfStation[13];
     node Line[13][50];
     int i, j, k;
-    int des;
+    int des = -1;
+    int dump;
     edge *tmp;
     IN = fopen("bgstations.txt", "r");
     fscanf(IN, "%d", &SumOfLine);
-
+    //Establish the map of station
     for (i = 0; i < SumOfLine; i++)
     {
         fscanf(IN, "%d%d", &line[i], &SumOfStation[i]);
@@ -63,38 +88,39 @@ void Init()
         {
             fscanf(IN, "%s%d", Line[i][j].Name, &(Line[i][j].flag));
         }
+    }
+    for (i = 0; i < SumOfLine; i++)
         for (j = 0; j < SumOfStation[i]; j++) //Set the map of stations
         {
+            dump = 0;
             if (Line[i][j].flag)
             {
                 for (k = 0; k < CountOfStation; k++)
                 {
-                    if (strcmp(Line[i][0].Name, map[k].Name) == 0)
+                    if (strcmp(Line[i][j].Name, map[k].Name) == 0)
                     {
-                        continue;
+                        dump = 1;
+                        break;
                     }
                 }
             }
-            strcpy(map[CountOfStation].Name, Line[i][j].Name);
-            map[CountOfStation].flag = Line[i][j].flag;
-            CountOfStation++;
+            if (!dump)
+            {
+                strcpy(map[CountOfStation].Name, Line[i][j].Name);
+                map[CountOfStation].flag = Line[i][j].flag;
+                CountOfStation++;
+            }
         }
-    }
+
+    //Deal with lines
     for (i = 0; i < SumOfLine; i++)
     {
         //Deal with the first station
-        tmp = (edge *)malloc(sizeof(edge));
+        tmp = (edge *) malloc(sizeof(edge));
         tmp->next = NULL;
         tmp->line = line[i];
         tmp->weight = 1;
-        for (k = 0; k < CountOfStation; k++)
-        {
-            if (strcmp(Line[i][1].Name, map[k].Name) == 0)
-            {
-                tmp->end = k;
-                break;
-            }
-        }
+        //The next station of the first one
         for (k = 0; k < CountOfStation; k++)
         {
             if (strcmp(Line[i][0].Name, map[k].Name) == 0)
@@ -103,11 +129,20 @@ void Init()
                 break;
             }
         }
+        for (k = 0; k < CountOfStation; k++)
+        {
+            if (strcmp(Line[i][1].Name, map[k].Name) == 0)
+            {
+                tmp->end = k;
+                break;
+            }
+        }
         tmp->next = map[des].list;
         map[des].list = tmp;
         //Deal with mid stations
         for (j = 1; j < SumOfStation[i] - 1; j++)
         {
+            //current station
             for (k = 0; k < CountOfStation; k++)
             {
                 if (strcmp(Line[i][j].Name, map[k].Name) == 0)
@@ -116,10 +151,11 @@ void Init()
                     break;
                 }
             }
-
-            tmp = (edge *)malloc(sizeof(edge));
+            //previous station
+            tmp = (edge *) malloc(sizeof(edge));
             tmp->line = line[i];
             tmp->weight = 1;
+            tmp->next = map[des].list;
             for (k = 0; k < CountOfStation; k++)
             {
                 if (strcmp(Line[i][j - 1].Name, map[k].Name) == 0)
@@ -128,12 +164,12 @@ void Init()
                     break;
                 }
             }
-            tmp->next = map[des].list;
             map[des].list = tmp;
-
-            tmp = (edge *)malloc(sizeof(edge));
+            //next station
+            tmp = (edge *) malloc(sizeof(edge));
             tmp->line = line[i];
             tmp->weight = 1;
+            tmp->next = map[des].list;
             for (k = 0; k < CountOfStation; k++)
             {
                 if (strcmp(Line[i][j + 1].Name, map[k].Name) == 0)
@@ -142,9 +178,9 @@ void Init()
                     break;
                 }
             }
-            tmp->next = map[des].list;
             map[des].list = tmp;
         }
+        //last station
         for (k = 0; k < CountOfStation; k++)
         {
             if (strcmp(Line[i][SumOfStation[i] - 1].Name, map[k].Name) == 0)
@@ -153,12 +189,12 @@ void Init()
                 break;
             }
         }
-        tmp = (edge *)malloc(sizeof(edge));
+        tmp = (edge *) malloc(sizeof(edge));
         tmp->line = line[i];
         tmp->weight = 1;
         for (k = 0; k < CountOfStation; k++)
         {
-            if (strcmp(Line[i][SumOfStation[i] - 1].Name, map[k].Name) == 0)
+            if (strcmp(Line[i][SumOfStation[i] - 2].Name, map[k].Name) == 0)
             {
                 tmp->end = k;
                 break;
@@ -166,16 +202,17 @@ void Init()
         }
         tmp->next = map[des].list;
         map[des].list = tmp;
+        //Out();
     }
 }
 
 void Dijkstra(char *Start, char *Des)
 {
-    int flag[CountOfStation];
-    int dis[CountOfStation];
-    int mid;
+    int flag[501];
+    int dis[501];
+    int mid = -1;
     int i, j;
-    int pos[CountOfStation];
+    int pos[501];
     int lowcost;
     int beg = -1, end = -1;
     edge *tmp;
@@ -192,6 +229,7 @@ void Dijkstra(char *Start, char *Des)
         if (beg >= 0 && end >= 0)
             break;
     }
+    startpoint = beg;
     destination = end;
 
     memset(flag, 0, sizeof(flag));
@@ -210,6 +248,7 @@ void Dijkstra(char *Start, char *Des)
         map[tmp->end].from = tmp;
         dis[tmp->end] = tmp->weight;
     }
+    dis[beg] = 0;
     flag[beg] = 1;
 
     while (1)
@@ -237,8 +276,11 @@ void Dijkstra(char *Start, char *Des)
             {
                 dis[tmp->end] = dis[mid] + tmp->weight;
                 map[tmp->end].from = tmp;
-                for (j = 0; j < pos[mid]; j++)
+                for (j = 0; j <= pos[mid]; j++)
+                {
                     path[tmp->end][j] = path[mid][j];
+                }
+                pos[tmp->end] = pos[mid];
             }
             tmp = tmp->next;
         }
